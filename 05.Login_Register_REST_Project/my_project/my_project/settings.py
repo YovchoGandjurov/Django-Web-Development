5defb0834be9f39cf4f03e528bee9107fcc66633
YovchoGandjurov <yovchoo@gmail.com>
2019-05-05 12:47:22 +0300

    added - reset password through terminal

--- 05.Login_Register_REST_Project/my_project/my_project/settings.py
+++ 05.Login_Register_REST_Project/my_project/my_project/settings.py
@@ -57,6 +57,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'my_project.urls'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -124,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_URL = 'rest_login'
