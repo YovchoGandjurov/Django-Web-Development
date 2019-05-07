9104c91996cae6b1243df9eb91b198d4a495cd81
YovchoGandjurov <yovchoo@gmail.com>
2019-05-07 23:28:24 +0300

    Added UserBooks

--- 04.Login_Register_Project/my_project/books/urls.py
+++ 04.Login_Register_Project/my_project/books/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.BookView.as_view(), name="books"),
     re_path('^(?P<pk>\d+)/$', views.BookDetail.as_view(), name="book-detail"),
+    re_path('^mine/$', views.UserBooks.as_view(), name='user-books')
 ]
--- 04.Login_Register_Project/my_project/books/views.py
+++ 04.Login_Register_Project/my_project/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class BookView(generic.ListView):
@@ -9,7 +10,29 @@ class BookView(generic.ListView):
     context_object_name = 'books'
 
 
-class BookDetail(generic.DetailView):
+class BookDetail(LoginRequiredMixin, generic.DetailView):
+    login_url = '/accounts/login/'
     model = Book
     template_name = 'book_detail.html'
     context_object_name = 'book'
+
+
+class UserBooks(LoginRequiredMixin, generic.ListView):
+    model = Book
+    template_name = 'book_list.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        user_id = int(self.request.user.id)
+
+        try:
+            author = Author.objects.all().filter(user__pk=user_id)[0]
+            books = Book.objects.all().filter(author=author.pk)
+            return books
+        except:
+            return []
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(UserBooks, self).get_context_data(**kwargs)
+        context['can_edit'] = True
+        return context
