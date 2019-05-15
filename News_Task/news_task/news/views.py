6cd1d2dc950530d07863784070f366a1e6ae30fa
YovchoGandjurov <yovchoo@gmail.com>
2019-05-15 12:49:49 +0300

    Added ordering and filtering

--- News_Task/news_task/news/views.py
+++ News_Task/news_task/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import News
 from .serializers import NewsSerializer
@@ -8,3 +10,23 @@ from .serializers import NewsSerializer
 class NewsList(generics.ListCreateAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    filter_backends = (filters.OrderingFilter, DjangoFilterBackend)
+    ordering_fields = ('date', 'title')
+    filterset_fields = ('date', 'title')
+
+    # Overriding the queryset
+    #
+    # def get_queryset(self):
+    #     date_query = self.request.query_params.get('date', None)
+    #     title_query = self.request.query_params.get('title', None)
+    #     print(self.request.query_params)
+    #     if date_query is not None and title_query is not None:
+    #         queryset = News.objects.all().filter(date=date_query,
+    #                                              title=title_query)
+    #     elif date_query is not None:
+    #         queryset = News.objects.all().filter(date=date_query)
+    #     elif title_query is not None:
+    #         queryset = News.objects.all().filter(title=title_query)
+    #     else:
+    #         queryset = News.objects.all()
+    #     return queryset
--- News_Task/news_task/news_task/settings.py
+++ News_Task/news_task/news_task/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'news'
 ]
@@ -122,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+}
