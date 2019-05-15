37000ff374979c231d93e108561b0eafb671444c
YovchoGandjurov <yovchoo@gmail.com>
2019-05-15 09:27:13 +0300

    Added models, serializers and views

--- News_Task/news_task/news/models.py
+++ News_Task/news_task/news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    date = models.DateField()
+    title = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    text = models.TextField()
--- News_Task/news_task/news/views.py
+++ News_Task/news_task/news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import News
+from .serializers import NewsSerializer
+
+
+class NewsList(generics.ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
